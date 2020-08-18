0d71528d4dd968874d44a4925788201e0d82b656
伊織 加藤 <ioriaka@outlook.jp>
2020-08-18 13:46:48 +0900

    a

--- ES Game Library/GameMain.cpp
+++ ES Game Library/GameMain.cpp
@@ -11,6 +11,34 @@ bool GameMain::Initialize()
 {
 	// TODO: Add your initialization logic here
 	WindowTitle(_T("ES Game Library"));
+	night = GraphicsDevice.CreateSpriteFromFile(_T("night.png"));
+	enemy = GraphicsDevice.CreateSpriteFromFile(_T("samurai.png"));
+	player = GraphicsDevice.CreateSpriteFromFile(_T("nin.png"), Color(255, 255, 255));
+	leftplayer = GraphicsDevice.CreateSpriteFromFile(_T("nin2.png"), Color(255, 255, 255));
+	kunai = GraphicsDevice.CreateSpriteFromFile(_T("kunai.png"), Color(255, 255, 255));
+	sword = GraphicsDevice.CreateSpriteFromFile(_T("katana.png"), Color(255, 255, 255));
+	sword2 = GraphicsDevice.CreateSpriteFromFile(_T("katana2.png"), Color(255, 255, 255));
+
+
+	chara_x = 0; chara_y = 514;
+	sword_x = chara_x + 100, sword_y = chara_y - 40;
+	sword2_x = chara_x - 100, sword2_y = chara_y - 40;
+
+	player_state = 0, jump_state = 0; hit_state = 0; shot_count = 0;
+	jumpspeed = 0; jumptime = 0; zahyou = 0; sword_flag = false;
+
+	enemy_x = 1100.0f;
+	enemy_y = 500.0f;
+	speed = 2.0f;
+
+	enemy_move_flg = false;
+
+	for (int i = 0; i < SHOT_MAX; i++)
+	{
+		shot_flg[i] = 0.0f;
+		shot_x[i] = 0.0f;
+		shot_y[i] = 0.0f;
+	}
 
 	return true;
 }
