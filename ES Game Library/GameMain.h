e6b7459e7d5e0f2e792df5b86aa9b9578d70a29a
伊織 加藤 <ioriaka@outlook.jp>
2020-08-25 16:42:39 +0900

    削除済み

--- ES Game Library/GameMain.h
+++ ES Game Library/GameMain.h
@@ -56,65 +56,9 @@ private:
 
 private:
 	// 変数宣言
-	SPRITE night;
-	float enemy_x, enemy_y, enemy2_x, enemy2_y;
-	float speed, speed2;
-	int enemy_move_flg;
-	int Game_Main_1_3_flg;
-	int Game_scene_flg;
-	int Game_scene_flg2;
-	SPRITE clear;
-	SPRITE ohiroma;
-	SPRITE jump;
-	SPRITE nin2, nin2Left;
-	SPRITE enemy;
-	SPRITE  player, leftplayer;
-	SPRITE	kunai, kunai2;
-	SPRITE floor, floor2, kaidan;
-	SPRITE Right, Up;
-	FONT text,text2;
-	SOUND open,close, endtaiko;
-
-	SPRITE Title;
-	SPRITE GameOver;
-	SPRITE Last;
-	SPRITE hp1;
-
-	SOUND se_End;
-	SOUND se_Attack;
-	SOUND se_start1;
-	SOUND se_start2;
-
-	MUSIC music;
-
-	float hp_x;
-	float hp_count;
-	int enemyhit_count;
-	int game_scene;
-	float chara_x, chara_y;
-	float time;
-	float frame;
-	float  chara2_x, chara2_y;
-	int player_state, jump_state, hit_state, shot_count, kunai_flag;
-	float jumpspeed, jumptime, zahyou;
-	float kunai_x, kunai_y, kunai2_x, kunai2_y;//攻撃用クナイ
-	float floor1_0x,floor1_1x, floor1_2x, kaidan1_x;
-	float floor2_0x, floor2_1x, floor2_2x, kaidan2_x;
-    int player_frame;
-
-	float floor1_1y;
-	float floor3_0x, floor3_1x, floor3_2x, kaidan3_x;
-	enum { SHOT_MAX = 1 };
-	int shot_flg[SHOT_MAX];
-	float shot_x[SHOT_MAX];
-	float shot_y[SHOT_MAX];
-	float makimono_x, makimono_y;
-	int ohiroma_flag, clear_flag;
+	
 
 
 	// 関数宣言
-	void steage2();
 	
-	void MainPlayer_LastStage();
-
 };
